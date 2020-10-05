08366e95a1efc3ab5844b4b47538e80c7f606bf1
zikram013 <32304189+zikram013@users.noreply.github.com>
2020-10-05 19:15:58 +0200

    Primera version

--- app/src/main/java/com/example/preguntados/MainActivity.java
+++ app/src/main/java/com/example/preguntados/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.preguntados;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i=new Intent(MainActivity.this,Pregunta1.class);
+        Button iniciarJuego = findViewById(R.id.iniciarJuego);
+        iniciarJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/preguntados/Pregunta2.java
@@ -0,0 +1,83 @@
+package com.example.preguntados;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.graphics.BlendMode.COLOR;
+
+public class Pregunta2 extends Activity implements View.OnClickListener {
+    private Button a,b,c,d,sig;
+    private int puntuacion=0;
+    private Intent i,recibe;
+    private Bundle bolsa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        a= findViewById(R.id.respuesta1);
+        b= findViewById(R.id.respuesta2);
+        c= findViewById(R.id.respuesta3);
+        d= findViewById(R.id.respuesta4);
+        sig= findViewById(R.id.siguientePregunta);
+        try{
+
+        }catch (Exception e){
+            Toast.makeText(this,"Error al recibir datos",Toast.LENGTH_SHORT).show();
+        }
+        recibe=getIntent();
+        bolsa=recibe.getExtras();
+        puntuacion=bolsa.getInt("calificacion");
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+        c.setOnClickListener(this);
+        d.setOnClickListener(this);
+        sig.setOnClickListener(this);
+        sig.setEnabled(false);
+
+    }
+
+    @Override
+    public void onClick(View arg0){
+        switch (arg0.getId()){
+            case R.id.respuesta4:
+            case R.id.respuesta3:
+            case R.id.respuesta2:
+                puntuacion+=0;
+                bloquearBoton();
+                break;
+            case R.id.respuesta1:
+                puntuacion+=1;
+                bloquearBoton();
+                break;
+            case R.id.siguientePregunta:
+                startActivity(i);
+            default:
+        }
+        i.putExtra("puntuacion",puntuacion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void bloquearBoton(){
+        a.setBackgroundColor(Color.GREEN);
+        b.setBackgroundColor(Color.RED);
+        c.setBackgroundColor(Color.RED);
+        d.setBackgroundColor(Color.RED);
+        a.setEnabled(false);
+        b.setEnabled(false);
+        c.setEnabled(false);
+        d.setEnabled(false);
+        sig.setEnabled(true);
+    }
+}
